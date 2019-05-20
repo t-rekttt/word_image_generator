f7e56adbe73cc8bc9b94d93bcbdea403eaacade9
T-Rekt <t.to.the.rekt@gmail.com>
2019-05-20 16:42:01 +0700

    Update

--- generate.py
+++ generate.py
@@ -4,20 +4,24 @@
 from docx import Document
 from docx.shared import RGBColor, Pt, Inches
 from PIL import Image
+from progressbar import progressbar
+import time
 
 image_path = '<path_to_your_image_file>'
 text_path = '<path_to_your_text_file>'
-output_path '<output_path.docx>'
+output_path = '<output_path.docx>'
 
 img = Image.open(image_path)
 text = open(text_path).read()
 
 width, height = img.size
+print('Width: {}, height: {}'.format(width, height))
+print('Total operations: {}'.format(width * height))
 
 document = Document()
 for section in document.sections:
-  section.page_width = Inches(11.69)
-  section.page_height = Inches(16.54)
+  section.page_width = Inches(16.54)
+  section.page_height = Inches(11.69)
 styles = document.styles['Normal']
 styles.font.size = Pt(1.5)
 styles.font.name = 'Arial Black'
@@ -26,12 +30,12 @@ paragraph_format.line_spacing = Pt(1)
 paragraph_format.space_before = Pt(0)
 paragraph_format.space_after = Pt(0)
 
-for j in range(height):
+for i in progressbar(range(height)):
   p = document.add_paragraph()
-  for i in range(width):
-    r, g, b = img.getpixel((i, j))
+  for j in range(width):
+    r, g, b = img.getpixel((j, i))
     run = p.add_run(
-      str(text[(i+j*width) % len(text)])
+      str(text[(j+i*width) % len(text)])
     )
     run.font.color.rgb = RGBColor(r, g, b)
     
